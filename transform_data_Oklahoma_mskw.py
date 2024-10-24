d213f261344559cebd11427a4022d55aa2b97e3d
Le Viet (FMI) <viet.le@fmi.fi>
2024-10-24 12:12:19 +0300

    transform and upload Oklahoma bp4, mskw

---
+++ transform_data_Oklahoma_mskw.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+import os
+import xarray as xr
+from functools import reduce
+
+# %%
+dir_in = r"C:\Users\le\OneDrive - Ilmatieteen laitos\WMO-DC\Oklahoma\mSKW2/"
+
+# %%
+# Define meta data
+operatorID = '049'
+airframeID = 'mskw'
+platform_name = 'mSKW'
+flight_id = 'Oklahoma_1000m_VP'
+
+# %%
+sub_dir = [f.path for f in os.scandir(dir_in) if f.is_dir()]
+save_path = sub_dir[-1] + '/'
+paths = [x + '/' for x in sub_dir[:-2]]
+
+for path in paths:
+    if "20240911_21.04" in path:
+        continue
+
+    
+    rename_dict = {
+        ' lat' : 'lat',
+        ' lon' : 'lon',
+        ' alt' : 'altitude',
+        'temp_bme': 'air_temperature',
+        'rh_bme' : 'relative_humidity',
+        'press_bme' : 'air_pressure',
+    }
+    
+    file_type = '/*.csv'
+    all_files = glob.glob(path + file_type)
+    data = []
+    for file in all_files:
+        df = pd.read_csv(file, sep=None, engine='python')
+        if 'date_time' in df.columns:
+            df['datetime'] = pd.to_datetime(df['date_time'])
+        else:
+            df['datetime'] = pd.to_datetime(df['date'] + ' '+ df['time'])
+        data.append(df)
+    data_merged = reduce(lambda left, right: pd.merge(left, right,
+                                                      on='datetime',
+                                                      how='inner'),
+                         data)
+    pd_time = data_merged['datetime']
+    
+    data_merged = data_merged.rename(rename_dict, axis=1)
+    data_merged = data_merged[[v for _, v in rename_dict.items()]]
+    
+    ds = xr.Dataset.from_dataframe(data_merged)
+    ds = ds.rename({'index':'obs'})
+    ref_time = np.datetime64('1970-01-01T00:00:00')
+    ds['time'] = (pd_time.values - ref_time) / np.timedelta64(1, 's')
+    
+    # Convert Air Temperature from Celsius to Kelvin
+    ds['air_temperature'] = ds['air_temperature'] + 273.15
+    
+    # Convert Air pressure to Pa
+    ds['air_pressure'] = 100 * ds['air_pressure']
+    
+    # Adding a REQUIRED Variable to the Dataset 
+    ds['humidity_mixing_ratio'] = np.nan
+    
+    # Adding attributes to variables in the xarray dataset
+    ds['time'].attrs = {'units': 'seconds since 1970-01-01T00:00:00', 'long_name': 'Time', '_FillValue': float('nan'), 'processing_level': ''}
+    ds['lat'].attrs = {'units': 'degrees_north', 'long_name': 'Latitude', '_FillValue': float('nan'), 'processing_level': ''}
+    ds['lon'].attrs = {'units': 'degrees_east', 'long_name': 'Longitude', '_FillValue': float('nan'), 'processing_level': ''}
+    ds['altitude'].attrs = {'units': 'meters', 'long_name': 'Altitude', '_FillValue': float('nan'), 'processing_level': ''}
+    ds['air_temperature'].attrs = {'units': 'Kelvin', 'long_name': 'Air Temperature', '_FillValue': float('nan'), 'processing_level': ''}
+    ds['humidity_mixing_ratio'].attrs = {'units': 'kg/kg', 'long_name': 'Humidity Mixing Ratio', '_FillValue': float('nan'), 'processing_level': ''}
+    ds['relative_humidity'].attrs = {'units': '%', 'long_name': 'Relative Humidity', '_FillValue': float('nan'), 'processing_level': ''}
+    ds['air_pressure'].attrs = {'units': 'Pa', 'long_name': 'Atmospheric Pressure', '_FillValue': float('nan'), 'processing_level': ''}
+        
+    # Add Global Attributes
+    ds.attrs['Conventions'] = "CF-1.8, WMO-CF-1.0"
+    ds.attrs['wmo__cf_profile'] = "FM 303-2024"
+    ds.attrs['featureType'] = "vertical_profile"
+    ds.attrs['platform_name'] = platform_name # example "GS_weatherhive"
+    ds.attrs['flight_id'] = flight_id # example: "JBCC_1500m_VP"
+    ds.attrs['processing_level'] = "raw"
+    
+    # Grab Initial timestamp of observations
+    timestamp_dt = pd.to_datetime(ds['time'].values[0], unit='s', origin='unix')
+    
+    # Format datetime object to desired format (YYYYMMDDHHMMSSZ)
+    formatted_timestamp = timestamp_dt.strftime('%Y%m%d%H%M%S') + 'Z'
+    
+    # Save to a NetCDF fileR
+    ds.to_netcdf(save_path + f'UASDC_{operatorID}_{airframeID}_{formatted_timestamp}.nc')
+
---
+++ upload_WMO_Oklahoma_bp4.py
@@ -0,0 +1,59 @@
+import boto3
+import os
+import glob
+
+# %%
+with open('UASDC_Participant_S3_Creds.txt', 'r') as f:
+    lines = [line.rstrip() for line in f]
+
+# access key sent separately 
+aws_access_key_id = lines[7]
+# secret access key sent separately
+aws_secret_access_key = lines[9]
+# bucket name
+bucket_name = lines[1]
+
+# %%
+# path to file
+local_filepaths = r"C:\Users\le\OneDrive - Ilmatieteen laitos\WMO-DC\Oklahoma\mavic2pro\bp4\WMO_ready/*.nc"
+
+# %%
+files = glob.glob(local_filepaths)
+# Create an S3 client
+s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id,
+                      aws_secret_access_key=aws_secret_access_key)
+
+for local_filepath in glob.glob(local_filepaths):
+    # s3_filepath = "049/" + os.path.basename(local_filepath)[6:]
+    s3_filepath = "049/" + os.path.basename(local_filepath)
+       # Upload the file to the S3 bucket
+    try:
+        s3.upload_file(local_filepath, bucket_name, s3_filepath)
+        print(f"File {local_filepath} uploaded to {bucket_name}/{s3_filepath}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+# %%
+# # Listing all files in the s3 bucket
+# try:
+#     response = s3.list_objects_v2(Bucket=bucket_name)
+#     if 'Contents' in response:
+#         print("Files in the bucket:")
+#         for item in response['Contents']:
+#             print(item['Key'])
+#     else:
+#         print("No files found in the bucket.")
+# except Exception as e:
+#     print(f"Failed to list files in the bucket: {e}")
+    
+# %%
+# Create a reusable Paginator
+paginator = s3.get_paginator('list_objects_v2')
+
+# Create a PageIterator from the Paginator
+page_iterator = paginator.paginate(Bucket=bucket_name)
+
+for page in page_iterator:
+    for item in page['Contents']:
+        if '/049/' in item['Key']:
+            print(item['Key'])
